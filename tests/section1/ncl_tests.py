a253429a5a62be31f558332d851dd82117d77dc1
U. Boscolo <uboscolo@gmail.com>
2015-06-23 13:25:11 -0400

    added new test section

---
+++ tests/section1/ncl_tests.py
@@ -0,0 +1,29 @@
+from nose.tools import *
+import os
+from ncl.ncl import DistributionDB
+
+def setup():
+    try:
+        os.remove("test.db")
+    except:
+        pass
+    assert_false(os.path.exists("test.db"))
+
+def teardown():
+    try:
+        os.remove("test.db")
+    except:
+        pass
+    assert_false(os.path.exists("test.db"))
+
+
+def test_distribution1():
+    db = DistributionDB("test.db")
+    assert_equal(db.cumulative_total, 0)
+    db.Destroy()
+
+def test_distribution2():
+    db = DistributionDB("test.db")
+    db.CreateTable()   
+    db.Destroy()
+
