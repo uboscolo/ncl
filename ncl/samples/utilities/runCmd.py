4d119670065bafc59e9db91e99791bd206c091dd
U. Boscolo <uboscolo@gmail.com>
2015-06-23 11:55:37 -0400

    reimplemented runCmd

--- ncl/samples/utilities/runCmd.py
+++ ncl/samples/utilities/runCmd.py
@@ -1,49 +1,118 @@
 #!/usr/bin/env python
 
 import sys
+import os
 import getpass
-import getopt
-from myconnect import *
+import optparse
+import xml.etree.ElementTree as ET
+from remote.connect.myconnect import *
+
+
+class Host(object):
+
+    def __init__(self):
+        self.name = None
+        self.prefix = None
+        self.number = None
+        self.domain = None
+        self.ip = None
+        self.username = None
+        self.password = None
+        self.connection = None
+
+    def GetHostName(self):
+        if not self.name:
+            self.name = self.prefix
+            if self.number:
+                self.name += str(self.number)
+            self.name += "." + self.domain
+        return self.name
+
 
 def main(argv):
-    try:
-        opts, args = getopt.getopt(sys.argv[1:], "hvp:", 
-            ["help", "verbose", "port="])
-    except getopt.GetoptError as err:
-        # print help information and exit:
-        print str(err) # will print something like "option -a not recognized"
-        print "Usage: %s [options] <target hostname>" % sys.argv[0]
-        sys.exit(2)
+    parser = optparse.OptionParser(usage="usage: %prog [options]")
+    parser.add_option('-v', '--verbose',
+                  dest="verbose",
+                  default=False,
+                  help="turn on verbosity",
+                  action="store_true",
+                  )
+    parser.add_option('-f', '--file',
+                  dest="file",
+                  help="read from xml file",
+                  action="store",
+                  )
+    parser.add_option('-p', '--port',
+                  dest="port",
+                  type="int",
+                  help="specify port number",
+                  action="store",
+                  )
+    parser.add_option('--virsh-console',
+                  dest="chew",
+                  default=False,
+                  help="turn on console reading",
+                  action="store_true",
+                  )
+    opts, remainder = parser.parse_args()
 
+    if len(remainder):
+        parser.error("wrong number of arguments")
+  
     options = {}
-    arglen = len(argv)
-    for o, a in opts:
-        if o in ("-v", "--verbose"):
-            options["verbose"] = True
-            arglen -= 1
-        elif o in ("-h", "--help"):
-            print "Usage: %s [options] <target hostname>" % sys.argv[0]
-            sys.exit()
-        elif o in ("-p", "--port"):
-            options["port"] = a
-            arglen -= 2
+    cmd_list = []
+    host_list = []
+    if opts.verbose:
+        options["verbose"] = opts.verbose
+    if opts.port:
+        options["port"] = opts.port
+    if opts.file:
+        xml_file = opts.file
+        if not os.path.exists(xml_file) or not os.path.isfile(xml_file):
+            print "invalid file %s" % xml_file
+            sys.exit(1)
+        tree = ET.parse(xml_file)
+        root_tag = tree.getroot()
+        assert root_tag.tag == "host"
+        # range should be optional
+        range = root_tag.attrib['range'].split("-")
+        for next_tag in root_tag:
+            if next_tag.tag == "user":
+                username = next_tag.attrib['name']
+            elif next_tag.tag == "password":
+                password = next_tag.attrib['name']
+            elif next_tag.tag == "command":
+                cmd_list.append(next_tag.attrib['name'])
+        for r in range:
+            if not r.isdigit():
+                print "invalid range %s, should be x-y" % range
+                sys.exit(1)
+            new_host = Host()
+            host_list.append(new_host)
+            new_host.prefix = root_tag.attrib['prefix']
+            new_host.number = int(r)
+            new_host.domain = root_tag.attrib['domain']
+            new_host.username = username
+            new_host.password = password
+
+    else:
+        #interactive
+        new_host = Host()
+        host_list.append(new_host)
+        new_host.name = raw_input('Please Enter a Hostname: ')
+        new_host.username = raw_input('Please Enter a User Name: ')
+        new_host.password = getpass.getpass('Please Enter a Password: ')
+        cmd_list.append(raw_input('Please Enter a Command: '))
+
+    for host in host_list:
+        hostname = host.GetHostName()
+        c = Connect(hostname, host.username, host.password, **options)
+        c.Open()
+        if opts.chew:
+            c.Chew(cmd_list[0])
         else:
-            assert False, "unhandled option"
-    # ...
-
-    if arglen != 1:
-        print "Usage: %s [options] <target hostname>" % sys.argv[0]
-        sys.exit(1)
-
-    hostname = sys.argv[len(argv)]
-    user = raw_input('Please Enter a User Name: ')
-    password = getpass.getpass('Please Enter a Password: ')
-    cmd_list = raw_input('Please Enter a Command  List: ')
-
-    c = Connect(hostname, user, password, **options)
-    c.Open()
-    c.Run(cmd_list)
-    c.Close()
+            c.Run(cmd_list)
+        c.Close()
 
 if __name__ == "__main__":
     main(sys.argv[1:])
